0c43a6c4fb27dd44192db18c340c767fcb390e07
Dj474 <toxap06@gmail.com>
2026-02-05 17:37:46 +0300

    init: video-process-service

---
+++ backend/video-process-service/src/test/java/org/videoprocessservice/VideoProcessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.videoprocessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProcessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
