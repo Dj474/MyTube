7fcac7ee861957787c79b2e88188f9cdfe0d7875
Dj474 <toxap06@gmail.com>
2026-02-08 14:55:45 +0300

    feat: resize video to 360p, 720p, 1080p

--- backend/video-process-service/src/main/java/org/videoprocessservice/service/kafka/KafkaConsumerService.java
+++ backend/video-process-service/src/main/java/org/videoprocessservice/service/kafka/KafkaConsumerService.java
@@ -4,18 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.videoprocessservice.other.record.VideoUploadEvent;
-import org.videoprocessservice.service.minio.StorageService;
+import org.videoprocessservice.service.video.ProcessService;
 
 @RequiredArgsConstructor
 @Service
 public class KafkaConsumerService {
 
-    private final StorageService storageService;
+    private final ProcessService processService;
 
     @KafkaListener(topics = "video-upload-topic", groupId = "video-processor-group")
     public void onVideoUpload(VideoUploadEvent event) {
         System.out.println("Получено видео для обработки: " + event.title());
-        storageService.processVideoTask(event.s3Key());
+        processService.processVideoTask(event.s3Key(), event.videoId());
     }
 
 
