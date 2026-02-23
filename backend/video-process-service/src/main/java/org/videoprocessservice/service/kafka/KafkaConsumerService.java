65703ac1ef0a244f9a7311ce5eba08f76ad4deba
Dj474 <toxap06@gmail.com>
2026-02-23 18:10:10 +0300

    fix: kafka listener factories

--- backend/video-process-service/src/main/java/org/videoprocessservice/service/kafka/KafkaConsumerService.java
+++ backend/video-process-service/src/main/java/org/videoprocessservice/service/kafka/KafkaConsumerService.java
@@ -15,13 +15,13 @@ public class KafkaConsumerService {
     private final VideoService videoService;
     private final SubscriptionService subscriptionService;
 
-    @KafkaListener(topics = "video-upload-topic", groupId = "video-processor-group")
+    @KafkaListener(topics = "video-upload-topic", groupId = "video-processor-group", containerFactory = "kafkaListenerContainerFactory")
     public void onVideoUpload(VideoUploadEvent event) {
         System.out.println("Получено видео для обработки: " + event.title());
         videoService.processVideo(event.videoId());
     }
 
-    @KafkaListener(topics = "user-subscription-topic", groupId = "video-processor-group")
+    @KafkaListener(topics = "user-subscription-topic", groupId = "video-processor-group", containerFactory = "kafkaListenerSubscriptionContainerFactory")
     public void onUserSubscription(UserSubscriptionEvent event) {
         System.out.println("Получена подписка для обработки: " + event.followerId() + " на " + event.authorId());
         subscriptionService.processSubscriptionEvent(event);
