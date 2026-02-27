463cd1a7a1a00c6ececb847105415e733cd7fc53
Dj474 <toxap06@gmail.com>
2026-02-27 20:02:22 +0300

    fix: fix exception handler

--- backend/comment-service/src/main/java/org/commentservice/exception/handler/GlobalExceptionHandler.java
+++ backend/comment-service/src/main/java/org/commentservice/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.commentservice.exception.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.commentservice.dto.error.ErrorResponseDtoOut;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
-import org.userservice.dto.error.ErrorResponseDtoOut;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
