560da91cfe6a7031112c0cd3902839523b29772e
Dj474 <toxap06@gmail.com>
2026-03-05 23:02:08 +0300

    init: proj

---
+++ backend/recommendation-search-service/src/main/java/org/recommendationsearchservice/RecommendationSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package org.recommendationsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationSearchServiceApplication.class, args);
+    }
+
+}
---
+++ backend/recommendation-search-service/src/test/java/org/recommendationsearchservice/RecommendationSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.recommendationsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
