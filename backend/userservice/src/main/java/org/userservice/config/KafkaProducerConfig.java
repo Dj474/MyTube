3cc247e05b2b9032f8f7ae8f5c32b9635e57c2d5
Dj474 <toxap06@gmail.com>
2026-03-10 16:32:02 +0300

    feat: userservice kafka production for recommendation

--- backend/userservice/src/main/java/org/userservice/config/KafkaProducerConfig.java
+++ backend/userservice/src/main/java/org/userservice/config/KafkaProducerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.userservice.other.record.kafka.UserForSearchRecord;
 import org.userservice.other.record.kafka.UserSubscriptionEvent;
 import org.apache.kafka.clients.producer.ProducerConfig;
 
@@ -32,6 +33,20 @@ public class KafkaProducerConfig {
     @Bean
     public KafkaTemplate<String, UserSubscriptionEvent>kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, UserForSearchRecord> producerForSearchFactory() {
+        Map<String, Object>configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, UserForSearchRecord>kafkaForSearchTemplate() {
+        return new KafkaTemplate<>(producerForSearchFactory());
     }
 
 }
--- backend/userservice/src/main/java/org/userservice/service/kafka/KafkaProducerService.java
+++ backend/userservice/src/main/java/org/userservice/service/kafka/KafkaProducerService.java
@@ -3,6 +3,7 @@ package org.userservice.service.kafka;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.userservice.other.record.kafka.UserForSearchRecord;
 import org.userservice.other.record.kafka.UserSubscriptionEvent;
 
 @Service
@@ -10,9 +11,14 @@ import org.userservice.other.record.kafka.UserSubscriptionEvent;
 public class KafkaProducerService {
 
     private final KafkaTemplate<String, UserSubscriptionEvent> kafkaTemplate;
+    private final KafkaTemplate<String, UserForSearchRecord> kafkaForSearchTemplate;
 
     public void sendUploadEvent(UserSubscriptionEvent event) {
         kafkaTemplate.send("user-subscription-topic", event);
+    }
+
+    public void sendSearchEvent(UserForSearchRecord event) {
+        kafkaForSearchTemplate.send("user-recomend-topic", event);
     }
 
 }
